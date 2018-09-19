edddb73ce27a023cd4ee13e64b95f0f06914455f
Craig Dunlop <craigdun9393@gmail.com>
2018-09-19 15:16:04 +0100

    created save album

--- db/console.rb
+++ db/console.rb
@@ -1,11 +1,20 @@
 require("pry")
 require_relative("../models/artist")
+require_relative("../models/albums")
 
 Artist.delete_all()
 
 artist1 = Artist.new({"name" => "Elton John"})
 
-  artist1.save()
+artist1.save()
+
+album1 = Album.new({
+  "name" => "Tiny Dancer",
+  "genre" => "Classic",
+  "artist_id" => artist1.id
+  })
+
+  album1.save()
 
 binding.pry
 nil
--- models/albums.rb
+++ models/albums.rb
@@ -5,12 +5,22 @@ class Album
 
   attr_accessor :name, :genre
   attr_reader :id
-  
+
   def initialize(options)
     @name = options["name"]
     @genre = options["genre"]
     @id = options["id"].to_i if options["id"]
     @artist_id = options["artist_id"].to_i
+  end
+
+  def save()
+      sql = "INSERT INTO albums (name, genre, artist_id)
+      values
+      ($1, $2, $3)
+      RETURNING *"
+      values =[@name, @genre, @artist_id]
+      results = SqlRunner.run(sql,values)
+      @id = results[0]["id"].to_i
   end
 
 
